c15b71d2199f04526a096ab482390819e0ef991e
kidass <kidass@163.com>
2013-09-06 17:50:11 +0800

    Contact code

---
+++ Contact/Common/Data/Contact/ContactData.vb
@@ -0,0 +1,39 @@
+﻿'----------------------------------------------------------------
+' Copyright (C) 2006-2016 Josco Software Corporation
+' All rights reserved.
+'
+' This source code is intended only as a supplement to Microsoft
+' Development Tools and/or on-line documentation. See these other
+' materials for detailed information regarding Microsoft code samples.
+'
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY 
+' OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT 
+' LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR 
+' FITNESS FOR A PARTICULAR PURPOSE.
+'----------------------------------------------------------------
+Option Strict On
+Option Explicit On
+
+Imports System
+Imports System.Data
+Imports System.Runtime.Serialization
+
+Namespace Xydc.Platform.Common.Data
+
+    '----------------------------------------------------------------
+    ' 命名空间：Xydc.Platform.Common.Data
+    ' 类名    ：ContactData
+    '
+    ' 功能描述：
+    '     定义与人员信息相关表的数据访问格式
+    '----------------------------------------------------------------
+    <System.ComponentModel.DesignerCategory("Contact"), SerializableAttribute()> Public Class ContactData
+        Inherits System.Data.DataSet
+
+
+
+    End Class
+
+End Namespace
+
+
